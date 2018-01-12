b17bf8489b7f18bb44768b2f45993d28a7929b02
Wataru Hashimoto <w0129akjm@gmail.com>
2018-01-12 16:55:27 +0900

    no message

--- func_response_surface.m
+++ func_response_surface.m
@@ -1,14 +1,12 @@
 function  value  = func_response_surface(x, cur_sample_num, omega, r, sample_point)
-%    global dimension cur_sample_num omega r sample_point
     value=0;
-    %d =zeros(10,cur_sample_num);
     for g=1:cur_sample_num
         value = value + omega(g) * ...
             exp(- sum((x - sample_point(:,g)).^2) / (r^2));
     end
+    %a =(x.' - ones(10,cur_sample_num)).^2;
     %value = sum(omega * exp(- sum((x - sample_point).^2) / (r^2)));
     %d = x - sample_point;
     %d = d / r^2;
     %value = sum(omega.'*(exp(-sum(d))).');
-    
 end
