93cbd6b8944f17c58a1bc6d4aa55852666343544
wataru129 <w0129akjm@gmail.com>
2018-01-18 02:20:33 +0900

    no message

--- APSO_kmeans_xclusta.m
+++ APSO_kmeans_xclusta.m
@@ -2,63 +2,59 @@
 particle  = 30;   %The number of Particle
 Iteration = 200;  % Max of reiteration
 %%%%%%%%%%%%%%%%%%%%%%
-if samp_kazu == ini_samp_kazu
-    x_ini = (1-(-1)).*rand(particle,dimension)+(-1);
-    v_ini = (1-(-1)).*rand(particle,dimension)+(-1);
-    idx             = kmeans(x,clusta);
-end
+
+x_init          = 5*IP(1:particle,1:dimension,T).';
+v_init          = IP(1:particle,1:dimension,T).';
+idx             = kmeans(x_init.',clusta);
 class =zeros(clusta,1);
-x_pso =zeros(clusta,dimension);
+x_pso =zeros(dimension,clusta);
 for c_index=1:clusta
 %%%%%%%%%%%%%%%%%%%%%%Initialization%%%%%%%%%%%%%%%%%%%%%%%%%
-    ct             = 1;
-    class(c_index) = size(find(idx == c_index),1);
-    xs              = zeros(class(c_index),dimension);
-    v              = zeros(class(c_index),dimension);
+    flag            = 0;
+    ct              = 1;
+    class(c_index)  = size(find(idx == c_index),1);
+    x_p             = zeros(dimension,class(c_index));
+    v_p             = zeros(dimension,class(c_index));
+    c1_ex           = 1.4955 * ones(1,class(c_index));
+    c2_ex           = 1.4955 * ones(1,class(c_index));
+    w_pso           = 0.729 * ones(dimension,class(c_index));
+    alpha           = zeros(1,class(c_index));
+    update_particle = zeros(class(c_index),dimension);
+    c1              = repelem(c1_ex,dimension,1) ;
+    c2              = repelem(c2_ex,dimension,1) ;
     for i=1:class(c_index)
         if idx(i)==c_index
-            x(ct,:)= x(i,:);
-            v(ct,:)= v(i,:);
+            x_p(:,ct)= x_init(:,i);
+            v_p(:,ct)= v_init(:,i);
             ct     = ct + 1;
         end
     end
-    c1_ex     = 1.4955 * ones(class(c_index),1);
-    c2_ex     = 1.4955 * ones(class(c_index),1);
-    c1 = repelem(c1_ex,1,dimension) ;
-    c2 = repelem(c2_ex,1,dimension) ;
-    w_pso     = 0.729 * ones(class(c_index)dimension);
-    alpha             = zeros(1,class(c_index));
-    flag              = 0;
-    update_particle   = zeros(dimension,class(c_index));
     pbest   = x_p;
     fpbest=zeros(1,class(c_index));
-    gbest=pbest(:,1);
-    fgbest  = Func_response_surface(gbest, cur_sample_num, omega, r, sample_point);
-    for i=1:class(c_index)
-        fpbest(i)=Func_response_surface(pbest(:,i), cur_sample_num, omega, r, sample_point);
-        if fpbest(i)<fgbest
-            gbest=pbest(:,i);
-            fgbest=fpbest(i);
-        end
-    end
+    fpbest=O(pbest.', cur_sample_num, omega, r, sample_point);
+    gx=O(pbest.', cur_sample_num, omega, r2, sample_point);
+    fpbest = fpbest+ rr*(max((-SEIYAKU-gx),0)).^2;
+    [fgbest ig] = min(fpbest);
+    gbest=pbest(:,ig);
 %%%%%%%%%%%%%%%%%%%%%%Initialization%%%%%%%%%%%%%%%%%%%%%%%%%
     for k=1:Iteration
-        temp_fpbest=zeros(1,class(c_index));
+        fx=zeros(1,class(c_index));
         gbest_ex = gbest * ones(1,class(c_index));
         rd1      = repelem(rand(1,class(c_index)),dimension,1);
         rd2      = repelem(rand(1,class(c_index)),dimension,1);
-        v_p = w_pso.*v_p + c1.*rd1.*(pbest-x_p) ...
-                  + c2.*rd2.*(gbest_ex-x_p);
+        v_p = w_pso.*v_p + c1.*rd1.*(pbest-x_p) + c2.*rd2.*(gbest_ex-x_p);
         x_p = x_p + v_p;
-        x_p(x_p > 5)  =5;
-        x_p(x_p < -5) = -5;
-        v_p(v_p > 5)  = 5;
-        v_p(v_p < -5) = -5;
+        x_p(x_p > 1)  = 1;
+        x_p(x_p < -1) = -1;
+        v_p(v_p > 1)  = 1;
+        v_p(v_p < -1) = -1;
+        fx = O(x_p.', cur_sample_num, omega, r, sample_point);
+        gx = O(x_p.', cur_sample_num, omega2, r, sample_point);
+        fx = fx + rr*(max((-SEIYAKU-gx),0)).^2;
         for i=1:class(c_index)
-            temp_fpbest(i)=Func_response_surface(x_p(:,i), cur_sample_num, omega, r, sample_point);
-            if(temp_fpbest(i)<fpbest(i))
-                pbest(:,i)=x_p(:,i);
-                fpbest(i) = temp_fpbest(i);
+            if(fx(i)<fpbest(i))
+                pbest(:,i) = x_p(:,i);
+                fpbest(i)  = fx(i);
             end
         end
         [now_best , ig] = min(fpbest);
@@ -85,6 +81,10 @@ for c_index=1:clusta
         c1 = repelem(c1_ex,dimension,1) ;
         c2 = repelem(c2_ex,dimension,1) ;
     end
-    x_pso(:,c_index) = gbest;
+    x_pso(:,c_index)        = gbest.';
+    y_pso(:,c_index)        = fgbest;
+    fgbest_suii(ix,c_index) = fgbest;
+    ix(c_index)             = ix(c_index)+1;
+    fx(c_index)             = O(x_pso(:,c_index),omega,seikiten);
+    gx(c_index)             = O(x_pso(:,c_index),omega2,seikiten);
 end
-result_pso(:,:,C)    = x_pso;
