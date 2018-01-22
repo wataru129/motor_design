cc3f02acce0dae00a7ec668c0b42203c5ef6a61e
wataru129 <w0129akjm@gmail.com>
2018-01-23 04:38:34 +0900

    no message

--- Add_syoki.m
+++ Add_syoki.m
@@ -1,17 +1,16 @@
 %%%%%% 初期点設定 %%%%%&%%%
-seikiten=syokiten(1:dimension,1:samp_kazu,e)';
+seikiten = syokiten(1:dimension,1:samp_kazu,e).';
 for i=1:samp_kazu                        %初期サンプル点設定
     for j=1:dimension
-            samp_ten_add(i,j) = ((p_max(j)+p_min(j))./2+((p_max(j)-p_min(j))./2).*(rand*2-1.));
+        samp_ten_add(i,j) = ((p_max(j)+p_min(j))./2+((p_max(j)-p_min(j))./2).*(rand*2-1.));
     end
 end
 if (C1==1) && (C==0)
     for j=1:dimension
         samp_ten_add(1,j) = samp_x(1,j);
-        seikiten(1,j) = (samp_x(1,j)-(p_max(j)+p_min(j))./2)./((p_max(j)-p_min(j))./2);
+        seikiten(1,j)     = (samp_x(1,j)-(p_max(j)+p_min(j))./2)./((p_max(j)-p_min(j))./2);
     end
-    C=C+1
 end
-seikiten1=seikiten;
+seikiten1    = seikiten;
 seikiten_add = seikiten;
-samp_ten=samp_ten_add;
+samp_ten     = samp_ten_add;
